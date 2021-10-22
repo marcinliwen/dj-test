34e11f81bcf8f325d7fe7cd0db81e9accd0a2033
liwenmarcin <marcin.liwen@sohoshop.pl>
2021-10-22 15:27:42 +0200

    menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,38 @@
 import React from "react"
 import './header.css'
 import { Link } from "gatsby";
-import Social from '../social'
+import Social from '../social';
+import Navbar from "../navbar";
+import Close from "../../assets/close.svg";
+
 
 export default function Header( {pageName, heroImg} ) {
+
+  const [menu, setMenu] = React.useState(false);
+  const toggleHamburger =()=>{setMenu(!menu)}
+
+  React.useEffect(() => {
+    // Zaktualizuj tytuł dokumentu korzystając z interfejsu API przeglądarki
+    if(menu){document.body.classList.add('menu-open')}
+    else{document.body.classList.remove('menu-open')}
+  });
   return (
     <header className="line-after">
-      <div className="menu-burger">
+      <div role="button" className="menu-burger" onClick={() => toggleHamburger() } onKeyDown={() => toggleHamburger()}>
           <span></span>
           <span></span>
           <span></span>
       </div>
       <Link to ="/" className="page-title">Piotr <span className="color-second">Szymański</span></Link>
       <Social />
+      <div className={`menu-wrapper  ${menu? 'open': ''}`}>
+        <button className="close-btn" onClick={() => toggleHamburger() } onKeyDown={() => toggleHamburger()}> 
+        <Close style={{ width: "45px", height: "45px" }} /></button>
+        <Navbar />
+        <Social />
+        <div className="menu-back"></div>
+
+      </div>
     </header>
   )
 }
